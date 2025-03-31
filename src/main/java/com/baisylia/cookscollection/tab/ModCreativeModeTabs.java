7d3588a0b7d04f5ac334f3fdb4e054eab9e6a1ba
Cassian Godsted <cassian@godsted.com>
2025-03-31 16:14:09 -0400

    Fix Creative Mode tab

--- src/main/java/com/baisylia/cookscollection/tab/ModCreativeModeTabs.java
+++ src/main/java/com/baisylia/cookscollection/tab/ModCreativeModeTabs.java
@@ -3,6 +3,7 @@ package com.baisylia.cookscollection.tab;
 import com.baisylia.cookscollection.CooksCollection;
 import com.baisylia.cookscollection.block.ModBlocks;
 import com.baisylia.cookscollection.item.ModItems;
+import com.baisylia.cookscollection.refabricated.RegUtils;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -16,12 +17,14 @@ public class ModCreativeModeTabs {
 
 
     public static final ResourceKey<CreativeModeTab> COOKSCOLLECTION_TAB_KEY = ResourceKey.create(BuiltInRegistries.CREATIVE_MODE_TAB.key(), CooksCollection.locate("cookscollections_tab"));
-    public static final CreativeModeTab COOKSCOLLECTION_TAB  = FabricItemGroup.builder()
+    public static final CreativeModeTab COOKSCOLLECTION_TAB = FabricItemGroup.builder()
             .icon(() -> new ItemStack(ModItems.LEMON.get()))
             .title(Component.translatable("creativetab.cookscollection.cookscollections_tab"))
             .build();
 
     public static void register() {
+        RegUtils.regTab("cookscollections_tab", ()->COOKSCOLLECTION_TAB);
+
         ItemGroupEvents.modifyEntriesEvent(COOKSCOLLECTION_TAB_KEY).register(output -> {
             output.accept(ModItems.LEMON.get());
             output.accept(ModItems.SALT.get());
