6c9e1c1f690362a012060f4bf5ae44bd60f77afe
Cassian Godsted <cassian@godsted.com>
2025-03-31 15:19:41 -0400

    Migrate worldgen, compostables, and fuel

--- fabric/src/main/java/com/baisylia/cookscollection/fabric/CooksCollectionFabric.java
+++ fabric/src/main/java/com/baisylia/cookscollection/fabric/CooksCollectionFabric.java
@@ -1,7 +1,12 @@
 package com.baisylia.cookscollection.fabric;
 
 import com.baisylia.cookscollection.CooksCollection;
+import com.baisylia.cookscollection.block.ModBlocks;
+import com.baisylia.cookscollection.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
+import vectorwing.farmersdelight.refabricated.CompostableHelper;
 
 public final class CooksCollectionFabric implements ModInitializer {
     @Override
@@ -12,5 +17,10 @@ public final class CooksCollectionFabric implements ModInitializer {
 
         // Run our common setup.
         CooksCollection.init();
+        CompostingChanceRegistry.INSTANCE.add(ModItems.FRIED_POTATO.get(), 0.45f);
+        CompostingChanceRegistry.INSTANCE.add(ModItems.LEMON.get(), 0.25f);
+        FuelRegistry.INSTANCE.add(ModBlocks.LEMON_LOG.get(), 50);
+        FuelRegistry.INSTANCE.add(ModBlocks.LEMON_SAPLING.get(), 50);
+        FuelRegistry.INSTANCE.add(ModBlocks.LEMON_WOOD.get(), 50);
     }
 }
